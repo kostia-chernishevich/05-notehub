835f04e1071b5fafcb4a5804b758053126e2f5b4
kostia <natashatonyk493@gmail.com>
2025-10-19 18:26:42 +0300

    fixed app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,9 +6,9 @@ import { Modal } from "../Modal/Modal";
 import { NoteForm } from "../NoteForm/NoteForm";
 import { SearchBox } from "../SearchBox/SearchBox";
 import { useDebounce } from "use-debounce";
-import { Toaster, toast } from "react-hot-toast";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { fetchNotes, deleteNote } from "../../services/noteService";
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { fetchNotes } from "../../services/noteService";
 
 export default function App() {
   const [page, setPage] = useState(1);
@@ -16,40 +16,22 @@ export default function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [search, setSearch] = useState("");
   const [debouncedSearch] = useDebounce(search, 500);
-  const queryClient = useQueryClient();
 
-
+  
   useEffect(() => {
     setPage(1);
   }, [debouncedSearch]);
 
   
-  const {
-    data,
-    isLoading,
-    isError,
-  } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["notes", page, perPage, debouncedSearch],
     queryFn: () => fetchNotes({ page, perPage, search: debouncedSearch }),
-    placeholderData: (prev) => prev,
-  });
-
-
-  const mutation = useMutation({
-    mutationFn: deleteNote,
-    onSuccess: () => {
-      toast.success("Note deleted");
-      queryClient.invalidateQueries({ queryKey: ["notes"] });
-    },
-    onError: () => {
-      toast.error("Error deleting note");
-    },
+    placeholderData: (prev) => prev, 
   });
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
-  
   const totalPages = data?.totalPages ?? 1;
 
   return (
@@ -61,18 +43,16 @@ export default function App() {
         </button>
       </header>
 
-     
+   
       {data?.notes && data.notes.length > 0 && (
         <NoteList
           notes={data.notes}
-          onDelete={(id) => mutation.mutate(id)}
-          isDeleting={mutation.isPending}
           isLoading={isLoading}
           isError={isError}
         />
       )}
 
-      
+    
       {totalPages > 1 && (
         <Pagination page={page} totalPages={totalPages} onChangePage={setPage} />
       )}
--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,26 +1,40 @@
 import css from "./NoteList.module.css";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteNote } from "../../services/noteService";
 import type { Note } from "../../types/note";
+import toast from "react-hot-toast";
 import { Loader } from "../Loader/Loader";
 
 interface NoteListProps {
   notes: Note[];
-  onDelete: (id: string) => void;
-  isDeleting?: boolean;
-  isLoading?: boolean;
-  isError?: boolean;
+  isLoading: boolean;
+  isError: boolean;
 }
 
-export function NoteList({
-  notes,
-  onDelete,
-  isDeleting,
-  isLoading,
-  isError,
-}: NoteListProps) {
+export function NoteList({ notes, isLoading, isError }: NoteListProps) {
+  const queryClient = useQueryClient();
+
+  
+  const mutation = useMutation({
+    mutationFn: deleteNote,
+    onSuccess: () => {
+      toast.success("Note deleted");
+      queryClient.invalidateQueries({ queryKey: ["notes"] }); 
+    },
+    onError: () => {
+      toast.error("Error deleting note");
+    },
+  });
+
   if (isLoading) return <Loader />;
+
+ 
   if (isError) return <p>Error loading notes</p>;
+
+ 
   if (!notes || notes.length === 0) return <p>No notes found</p>;
 
+  
   return (
     <ul className={css.list}>
       {notes.map((note) => (
@@ -30,11 +44,11 @@ export function NoteList({
           <div className={css.footer}>
             <span className={css.tag}>{note.tag}</span>
             <button
-              disabled={isDeleting}
-              onClick={() => onDelete(note.id)}
+              disabled={mutation.isPending}
+              onClick={() => mutation.mutate(note.id)}
               className={css.button}
             >
-              {isDeleting ? "Deleting..." : "Delete"}
+              {mutation.isPending ? "Deleting..." : "Delete"}
             </button>
           </div>
         </li>
